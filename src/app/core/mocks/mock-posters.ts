d1f9bcf155e79bff01027640048a042e22f904fd
Eva Lieu <itsevalieu@gmail.com>
2020-07-10 21:41:44 -0700

    Fleshing out mock data

--- src/app/core/mocks/mock-posters.ts
+++ src/app/core/mocks/mock-posters.ts
@@ -2,12 +2,13 @@ import { Poster } from '@core/models/poster';
 export const POSTERS: Poster[] = [
   {
     id: 1,
-    title: "Cat's Cradle1",
+    title: "Cat's Cradle",
     subtitle: "Just a cat in it's cradle.",
     short_description:
       'This be a short description of what the oneshot is supposed to be about. Guess it should be a paragraph long or something like that.',
-    long_description: 'So and so series of paragraphs. Blah.',
-    difficulty_level: 4,
+    long_description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    difficulty_level: 5,
     type_id: 1,
     author: 'Eva Lieu',
     images: [
@@ -18,14 +19,15 @@ export const POSTERS: Poster[] = [
   },
   {
     id: 2,
-    title: "Cat's Cradle2",
-    subtitle: "Just a cat in it's cradle.",
+    title: 'Bold Misty',
+    subtitle: 'What are you excited about that’s going to happen soon?',
     short_description:
-      'This be a short description of what the oneshot is supposed to be about. Guess it should be a paragraph long or something like that.',
-    long_description: 'So and so series of paragraphs. Blah.',
+      'Have you ever crossed a river as big as this one? Have you ever crossed a river as big as this one?',
+    long_description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. \n Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n\n Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     difficulty_level: 4,
-    type_id: 1,
-    author: 'Eva Lieu',
+    type_id: 2,
+    author: 'Marten Frizado',
     images: [
       { src: 'www.google.com', alt: 'Sample pic' },
       { src: 'www.google.com', alt: 'Sample pic' },
@@ -34,14 +36,116 @@ export const POSTERS: Poster[] = [
   },
   {
     id: 3,
-    title: "Cat's Cradle3",
-    subtitle: "Just a cat in it's cradle.",
+    title: 'The Missing Swords',
+    subtitle: 'He never wants to go on a rollercoaster again!',
+    short_description:
+      'I want racecars to be legal for average people. I want racecars to be legal for average people.',
+    long_description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    difficulty_level: 8,
+    type_id: 3,
+    author: 'Marco Boland',
+    images: [
+      { src: 'www.google.com', alt: 'Sample pic' },
+      { src: 'www.google.com', alt: 'Sample pic' },
+    ],
+    link: 'www.bing.com',
+  },
+  {
+    id: 4,
+    title: 'Nobody of Edge',
+    subtitle: 'That’s too bad you should have thought of that before!',
+    short_description:
+      'One constellation that most people know is the Big Dipper. One constellation that most people know is the Big Dipper.',
+    long_description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    difficulty_level: 3,
+    type_id: 4,
+    author: 'Karia McCune',
+    images: [
+      { src: 'www.google.com', alt: 'Sample pic' },
+      { src: 'www.google.com', alt: 'Sample pic' },
+    ],
+    link: 'www.bing.com',
+  },
+  {
+    id: 5,
+    title: 'Boyfriend of Moons',
+    subtitle: 'If you could live anywhere, where would you live and why?',
+    short_description:
+      'Animal print adds a nice touch to plain outfits. Animal print adds a nice touch to plain outfits.',
+    long_description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    difficulty_level: 5,
+    type_id: 1,
+    author: 'Sonnie Giertych',
+    images: [
+      { src: 'www.google.com', alt: 'Sample pic' },
+      { src: 'www.google.com', alt: 'Sample pic' },
+    ],
+    link: 'www.bing.com',
+  },
+  {
+    id: 6,
+    title: 'The Something of the Souls',
+    subtitle: "Never do that again or I won't be your friend!",
+    short_description:
+      'He never wants to go on a rollercoaster again! He never wants to go on a rollercoaster again! He never wants to go on a rollercoaster again!',
+    long_description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    difficulty_level: 16,
+    type_id: 2,
+    author: 'Miriam Wykoff',
+    images: [
+      { src: 'www.google.com', alt: 'Sample pic' },
+      { src: 'www.google.com', alt: 'Sample pic' },
+    ],
+    link: 'www.bing.com',
+  },
+  {
+    id: 7,
+    title: 'Window in the Slaves',
+    subtitle: 'He never wants to go on a rollercoaster again!',
+    short_description:
+      'What is your favorite toy to play with or what toy do you wish you had?',
+    long_description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. \n Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n\n Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. \nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    difficulty_level: 9,
+    type_id: 3,
+    author: 'Crawford Rau',
+    images: [
+      { src: 'www.google.com', alt: 'Sample pic' },
+      { src: 'www.google.com', alt: 'Sample pic' },
+    ],
+    link: 'www.bing.com',
+  },
+  {
+    id: 8,
+    title: "The Past's Flowers",
+    subtitle: 'If you could go anywhere on vacation, where would you go?',
+    short_description: 'Sarah will throw a big party with a lot of music.',
+    long_description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    difficulty_level: 13,
+    type_id: 4,
+    author: 'Kevyn Tien',
+    images: [
+      { src: 'www.google.com', alt: 'Sample pic' },
+      { src: 'www.google.com', alt: 'Sample pic' },
+    ],
+    link: 'www.bing.com',
+  },
+  {
+    id: 9,
+    title: 'Magnificent Thief',
+    subtitle: 'A big crowd gathered at the scene of the fire.',
     short_description:
       'This be a short description of what the oneshot is supposed to be about. Guess it should be a paragraph long or something like that.',
-    long_description: 'So and so series of paragraphs. Blah.',
-    difficulty_level: 4,
+    long_description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. \n Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n\n Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. \n Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    difficulty_level: 15,
     type_id: 1,
-    author: 'Eva Lieu',
+    author: 'Hannibal Groleau',
     images: [
       { src: 'www.google.com', alt: 'Sample pic' },
       { src: 'www.google.com', alt: 'Sample pic' },
